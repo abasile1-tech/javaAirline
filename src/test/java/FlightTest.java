35f6c8c47b40f7086b0a65107c10bd886ac04c6f
John <ashton.basile@gmail.com>
2023-09-24 21:02:34 +0100

    I am wrote some tests for the Flight class as part of TDD.

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -35,31 +35,61 @@ public class FlightTest {
 
     private Plane plane;
 
-    private FlightNumber flightNumber;
-
-    private Airport destinationAirport;
-
-    private Airport departureAirport;
-
-    private String departureTime;
-
     @Before
     public void before() {
-
+        passenger1 = new Passenger("John", 1);
+        passenger2 = new Passenger("Steven", 2);
+        passenger3 = new Passenger("Julia", 4);
+        passenger4 = new Passenger("Juanita", 1);
+        passenger5 = new Passenger("Vern", 3);
+        passenger6 = new Passenger("Rita",4);
+        passenger7 = new Passenger("Noelle",3);
+        passenger8 = new Passenger("Holly",2);
+        passenger9 = new Passenger("Simon",2);
+        passenger10 = new Passenger("Roger",0);
+        passenger11 = new Passenger("Nicole",1);
+        passenger12 = new Passenger("Eugene",1);
+        passenger13 = new Passenger("Marcella",1);
+        passengers = new ArrayList<>();
+        pilot = new Pilot("Leslie", Rank.CAPTAIN, "5579283");
+        cabinCrewMember1 = new CabinCrewMember("Ana", Rank.FLIGHTATTENDANT);
+        cabinCrewMember2 = new CabinCrewMember("Roger", Rank.FLIGHTATTENDANT);
+        cabinCrewMembers = new ArrayList<>();
+        cabinCrewMembers.add(pilot);
+        cabinCrewMembers.add(cabinCrewMember1);
+        cabinCrewMembers.add(cabinCrewMember2);
+        plane = new Plane(PlaneType.ANTONOVaN2);
+        flight = new Flight(pilot, cabinCrewMembers, plane, FlightNumber.FR756, Airport.EDI, Airport.LAS, "12:30 pm");
     }
 
     @Test
     public void canGetNumEmptySeats() {
-
+        assertEquals(12, flight.getNumEmptySeats());
     }
 
     @Test
     public void canBookPassengerIfNotFull() {
-
+        flight.addPassenger(passenger1);
+        assertEquals(passenger1, flight.getPassengers().get(0));
     }
 
     @Test
     public void canNotBookPassengerIfFull() {
-
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger3);
+        flight.addPassenger(passenger4);
+        flight.addPassenger(passenger5);
+        flight.addPassenger(passenger6);
+        flight.addPassenger(passenger7);
+        flight.addPassenger(passenger8);
+        flight.addPassenger(passenger9);
+        flight.addPassenger(passenger10);
+        flight.addPassenger(passenger11);
+        flight.addPassenger(passenger12);
+        assertEquals(12, flight.getPassengers().size());
+        assertEquals(false, flight.addPassenger(passenger13));
+        flight.addPassenger(passenger13);
+        assertEquals(12, flight.getPassengers().size());
     }
 }
