07116fc6e8ed8d0fa796bd337d42bf431e53501c
John <ashton.basile@gmail.com>
2023-09-24 21:16:45 +0100

    I wrote a test for the CabinCrewMember as part of tdd.

--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,6 +1,21 @@
+import org.example.CabinCrewMember;
+import org.example.Rank;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class CabinCrewMemberTest {
+    private CabinCrewMember cabinCrewMember;
+    private String message;
+
+    @Before
+    public void before() {
+        cabinCrewMember = new CabinCrewMember("Holly", Rank.FIRSTOFFICER);
+    }
+
+    @Test
+    public void canRelayMessageToPassenger() {
+        message = "Please fasten your seatbelts";
+        assertEquals(message, cabinCrewMember.relayMessageToPassenger(message));
+    }
 }
