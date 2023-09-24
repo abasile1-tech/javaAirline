9a8dc23794ed44e0a605098ec2e7c75469ff3650
John <ashton.basile@gmail.com>
2023-09-24 21:11:20 +0100

    I wrote a test for the Pilot class as part of tdd.

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,5 +1,17 @@
+import org.example.Pilot;
+import org.example.Rank;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 public class PilotTest {
+    private Pilot pilot;
+    @Before
+    public void before() {
+        pilot = new Pilot("Jenna", Rank.CAPTAIN, "2005467");
+    }
+
+    @Test
+    public void canFlyThePlane() {
+        assertEquals("I am flying the plane", pilot.flyThePlane());
+    }
 }
