308166a2b39758743896e2ffc60dc359c9c6ed9d
John <ashton.basile@gmail.com>
2023-09-24 19:53:35 +0100

    I am writing the tests for the Flight class as part of TDD.

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,65 @@
+import org.example.*;
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    private Flight flight;
+    private Pilot pilot;
+    private CabinCrewMember cabinCrewMember1;
+    private CabinCrewMember cabinCrewMember2;
+
+    private ArrayList<CabinCrewMember> cabinCrewMembers;
+
+    private Passenger passenger1;
+    private Passenger passenger2;
+    private Passenger passenger3;
+    private Passenger passenger4;
+    private Passenger passenger5;
+    private Passenger passenger6;
+    private Passenger passenger7;
+    private Passenger passenger8;
+    private Passenger passenger9;
+    private Passenger passenger10;
+
+    private Passenger passenger11;
+
+    private Passenger passenger12;
+
+    private Passenger passenger13;
+
+    private ArrayList<Passenger> passengers;
+
+    private Plane plane;
+
+    private FlightNumber flightNumber;
+
+    private Airport destinationAirport;
+
+    private Airport departureAirport;
+
+    private String departureTime;
+
+    @Before
+    public void before() {
+
+    }
+
+    @Test
+    public void canGetNumEmptySeats() {
+
+    }
+
+    @Test
+    public void canBookPassengerIfNotFull() {
+
+    }
+
+    @Test
+    public void canNotBookPassengerIfFull() {
+
+    }
 }
